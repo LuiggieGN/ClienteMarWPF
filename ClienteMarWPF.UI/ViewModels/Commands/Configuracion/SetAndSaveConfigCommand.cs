397fc8efd35e55b54488c6389ceaf49c11add453
Javier Martinez <44730875+JavierMartinez07@users.noreply.github.com>
2020-11-10 07:54:07 -0400

    Configuracion Ready

---
+++ ClienteMarWPF.UI/ViewModels/Commands/Configuracion/SetAndSaveConfigCommand.cs
@@ -0,0 +1,42 @@
+﻿using ClienteMarWPF.UI.Modules.Configuracion;
+using ClienteMarWPF.UI.State.LocalClientSetting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClienteMarWPF.UI.ViewModels.Commands.Configuracion
+{
+    public class SetAndSaveConfigCommand: ActionCommand
+    {
+        private readonly ConfiguracionViewModel ViewModel;
+        private readonly ILocalClientSettingStore LocalConfig;
+
+        public SetAndSaveConfigCommand(ConfiguracionViewModel viewModel, ILocalClientSettingStore localClient) : base()
+        {
+            ViewModel = viewModel;
+            LocalConfig = localClient;
+
+            Action<object> comando = new Action<object>(SaveConfig);
+            base.SetAction(comando);
+        }
+
+        private void SaveConfig(object parametro)
+        {
+            try
+            {
+                var settingDTO = new Domain.Models.Dtos.LocalClientSettingDTO {
+                    BancaId = ViewModel.BancaID,
+                    Direccion = ViewModel.Direccion,
+                    Tickets = ViewModel.Ticket
+                };
+
+                LocalConfig.WriteDesktopLocalSetting(settingDTO);
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+    }
+}
