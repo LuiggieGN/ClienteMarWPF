9a73b9da31d3e8ab4cb594efd0ee11ec86b87392
Luiggie Javier Nuñez Gomez <luiggie.nunez@outlook.com>
2021-03-23 09:31:58 -0400

    Updates

---
+++ ClienteMarWPF.UI/Views/Controls/SpinnerPistonAlt.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClienteMarWPF.UI.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for SpinnerPiston.xaml
+    /// </summary>
+    public partial class SpinnerPistonAlt : UserControl
+    {
+        public SpinnerPistonAlt()
+        {
+            InitializeComponent();
+        }
+    }
+}
