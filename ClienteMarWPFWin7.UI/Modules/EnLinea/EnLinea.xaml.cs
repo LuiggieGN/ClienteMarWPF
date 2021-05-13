0eefb1e441600e51bfe79779f62b6a3daf927351
Noe eduardo Medina henriquez <noeeduardomedinahenriquez@gmail.com>
2021-05-13 12:29:29 -0400

    Integracion WEB BROSWER

---
+++ ClienteMarWPFWin7.UI/Modules/EnLinea/EnLinea.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClienteMarWPFWin7.UI.Modules.EnLinea
+{
+    /// <summary>
+    /// Lógica de interacción para EnLinea.xaml
+    /// </summary>
+    public partial class EnLinea : UserControl
+    {
+        public EnLinea()
+        {
+            InitializeComponent();
+        }
+
+        //private void Window_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        web.Source = new Uri("https://www.google.com/");
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //    }
+        //   // Loaded = "Window_Loaded"
+        //}
+    }
+}
