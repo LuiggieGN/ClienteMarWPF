432f8133f0f3ee4b2e18697b08873db8936f5494
Luiggie Javier Nuñez Gomez <luiggie.nunez@outlook.com>
2021-07-09 16:02:37 -0400

    Pega Mas

--- ClienteMarWPFWin7.UI/Modules/JuegaMas/JuegaMasViewModel.cs
+++ ClienteMarWPFWin7.UI/Modules/JuegaMas/JuegaMasViewModel.cs
@@ -1,12 +1,14 @@
 ﻿using ClienteMarWPFWin7.UI.ViewModels.Base;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
+ 
 namespace ClienteMarWPFWin7.UI.Modules.JuegaMas
 {
     public class JuegaMasViewModel : BaseViewModel
     {
+
+        #region Campos
+        
+        #endregion
+
         public JuegaMasViewModel()
         {
 
