a6f5f65819d7af10d0974ca283398186b69dd6b5
Luiggie Javier Nuñez Gomez <luiggie.nunez@outlook.com>
2021-07-16 11:20:32 -0400

    Pega Mas Parte4

--- ClienteMarWPFWin7.UI/Modules/PegaMas/PegaMasView.xaml.cs
+++ ClienteMarWPFWin7.UI/Modules/PegaMas/PegaMasView.xaml.cs
@@ -33,6 +33,26 @@ namespace ClienteMarWPFWin7.UI.Modules.PegaMas
             {
                 vm.RemoverJugadasCommand?.Execute(null);
             }
+            else if (vm != null && (e.Key == Key.F12 || e.Key == Key.Add))
+            {
+                vm.VenderCommand?.Execute(null);
+            }
+            else if (e.Key == Key.Right ||
+                     e.Key == Key.Left)
+            {
+                e.Handled = true;
+                Seleccionar_Entrada_Segun_Direccion(direccionAsc: (e.Key == Key.Right) ? true : false);
+            }
+        }
+
+
+        private void ModuloKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Tab)
+            {
+                e.Handled = true;
+                Seleccionar_Entrada_Segun_Direccion(direccionAsc:  true  );
+            }
         }
 
         private void ModuloCargado(object sender, RoutedEventArgs e)
@@ -51,11 +71,52 @@ namespace ClienteMarWPFWin7.UI.Modules.PegaMas
                 vm.RemoverSoloUnaJugadaCommand?.Execute(objEnFila.Id);
             }
         }
+        
+        private void Seleccionar_Entrada_Segun_Direccion(bool direccionAsc) 
+        {
+            //  direccionAsc => 0 = Anterior 1 = Siguiente 
 
+            var entradas = new TextBox[] { EntradaD1, EntradaD2, EntradaD3, EntradaD4, EntradaD5 };
+
+            for (int i = 0; i < entradas.Length; i++)
+            {
+                if (entradas[i].IsFocused)
+                {
+                    if (direccionAsc)
+                    {
+                        if (i == entradas.Length - 1)
+                        {
+                            SeleccionarTodoElContenido(EntradaD1); break;
+                        }
+                        else
+                        {
+                            SeleccionarTodoElContenido(entradas[i + 1]); break;
+                        }
+                    }
+                    else
+                    {
+                        if (i == 0)
+                        {
+                            SeleccionarTodoElContenido(EntradaD5); break;
+                        }
+                        else
+                        {
+                            SeleccionarTodoElContenido(entradas[i - 1]); break;
+                        }
+                    }
+
+                }//fin de If
+
+            }//fin  de For
+        }
+
+        private void SeleccionarTodoElContenido(TextBox entrada) 
+        {
+            entrada.Focus();
+            entrada.SelectAll();
+        }
 
         private void IniciarFoco() => EntradaD1.Focus();
-
-
 
 
     }// Clase
